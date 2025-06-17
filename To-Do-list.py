2991eb0532ad867fa98df292ece224a2d089f190
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-17 20:31:41 +0200

    update project

--- To-Do-list.py
+++ To-Do-list.py
@@ -1,9 +1,35 @@
 Task = []
-num = int(input("How many tasks do you want to add? "))
+TotalTask=0
+condition=True
+def add():
+    global TotalTask
+    num = int(input("How many tasks do you want to add? "))
+    for i in range(num):
+        ADD = input(f"Enter task {i + 1}: ")
+        Task.append(ADD)
+        TotalTask += 1
 
-for i in range(num):
-    ADD = input(f"Enter task {i + 1}: ")
-    Task.append(ADD)
 
-for index, Task in enumerate(Task, start=1):
-    print(f"Task {index}: ",Task)
+def show_task():
+    if(TotalTask > 0):
+        for index, Task in enumerate(Task, start=1):
+            print(f"Task {index}: ",Task)
+    else:
+        print("No Task Founded")
+
+def remove():
+     show_task()
+
+
+while(condition):
+    chose = int(input("Enter your choice (1=Add, 2=Show, 3=Remove): "))
+
+    match chose:
+        case 1:
+            add()
+        case 2:
+            show_task()
+        case 3:
+            remove()
+        case _:
+            print("Lol Bro")
