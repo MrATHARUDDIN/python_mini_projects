fba3791339eb9e393904709cae7a56bdce9d4029
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-19 13:11:50 +0200

    leraing file and a new project

---
+++ scope_resolution.py
@@ -0,0 +1,26 @@
+# variable scope = where a variable is visiable and accessible
+# scope resolution = (LEGB) local -> Enclosed -> GLobal -> Built-in
+
+b=2
+def fun1():
+    a=1
+    print(a)
+
+def fun2():
+    print(b)
+
+# here in fun1 "a" is local variable
+# here in fun2 "b" is a Global variable
+
+
+# scope resolution is a advance concept where 
+# the permmion of using variable is goes in this order
+# local -> Enclosed -> GLobal -> Built-in
+x=3 # global
+def fun3():
+    x=1 # Enclose
+    def fun4():
+        x=2 # local
+        print(x)
+    fun4()
+fun3()
