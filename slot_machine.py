4f59996cf4d5be6d9302180367063b5656640dbc
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-20 20:07:56 +0200

    update

---
+++ slot_machine.py
@@ -0,0 +1,25 @@
+# python slot Machine
+
+def spin_row():
+    pass
+
+def print_row():
+    pass
+
+def payout():
+    pass
+
+def main():
+    balance = 100
+
+    print("************************")
+    print("Welcome to py Slots")
+    print("symbols : 🍒🍉🍋🔔⭐")
+    print("************************")
+
+    while balance >0:
+        print("Current Balance : ${balance}")
+        bet = int(input(" Enter Your bet ammount "))
+
+if __name__ == "_main_":
+    main()
