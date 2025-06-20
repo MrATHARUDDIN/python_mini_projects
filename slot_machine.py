11eb6d2817ebf1b2c17cda7adf36abd3fd5fddfa
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-20 20:28:08 +0200

    update

--- slot_machine.py
+++ slot_machine.py
@@ -1,7 +1,18 @@
-# python slot Machine
+import random
+
+
+
+payout_multipliers = {
+    "🍒": 2,
+    "🍉": 3,
+    "🍋": 5,
+    "🔔": 10,
+    "⭐": 20
+}
 
 def spin_row():
-    pass
+   symbols = ["🍒", "🍉", "🍋", "🔔", "⭐"]
+   return [random.choice(symbols) for _ in range(3)]
 
 def print_row():
     pass
@@ -13,13 +24,31 @@ def main():
     balance = 100
 
     print("************************")
-    print("Welcome to py Slots")
-    print("symbols : 🍒🍉🍋🔔⭐")
+    print("🎰 Welcome to Py Slots! 🎰")
+    print("Symbols: 🍒  🍉  🍋  🔔  ⭐")
     print("************************")
 
-    while balance >0:
-        print("Current Balance : ${balance}")
-        bet = int(input(" Enter Your bet ammount "))
+    while balance > 0:
+        print(f"\nCurrent Balance: ${balance}")
+        bet_input = input("Enter your bet amount: ")
 
-if __name__ == "_main_":
+        if not bet_input.isdigit():
+            print("❌ Please enter a valid number.")
+            continue
+
+        bet = int(bet_input)
+
+        if bet <= 0:
+            print("❌ Bet must be greater than zero.")
+            continue
+        if bet > balance:
+            print("❌ Insufficient balance.")
+            continue
+        balance -= bet
+        row = spin_row()
+        print(row)
+        
+    print("💸 Game Over! You ran out of money.")
+
+if __name__ == "__main__":
     main()
