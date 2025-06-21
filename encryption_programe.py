56aa45930d277d218afe56d0765b6c0398f40b2a
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-21 21:41:22 +0200

    New Update

--- encryption_programe.py
+++ encryption_programe.py
@@ -8,5 +8,30 @@ chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy() # Make a copy
 
-print(f"chars : {chars}")
-print(f"Keys : {key}")
+
+random.shuffle(key) # it will change the value randomly 
+# print(f"chars : {chars}")
+# print(f"Keys : {key}")
+
+
+#Entrypted
+text_en = input("Enter a message to encrypt: ")
+cipher_text = " "
+
+for letter in text_en:
+    index = chars.index(letter)
+    cipher_text +=key[index]
+
+print(f"Orginale message {text_en}")
+print(f"Encrypted message {cipher_text}")
+
+# Decrypted
+cipher_text = input("Enter a message to Decrypted: ")
+text_en = " "
+
+for letter in cipher_text:
+    index = key.index(letter)
+    text_en += chars[index]
+
+print(f"Orginale message {cipher_text}")
+print(f"Decrypted message {text_en}")
