9643f8558546a7c95f2db3ebe9af0163a5d25287
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-21 21:29:36 +0200

    add some comments

--- encryption_programe.py
+++ encryption_programe.py
@@ -2,9 +2,11 @@ import random
 import string
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
+#This line builds a string of all possible characters you might want to use in encoding or encryption.
 
+# Converting to a List (type casting)
 chars = list(chars)
-key = chars.copy()
+key = chars.copy() # Make a copy
 
 print(f"chars : {chars}")
 print(f"Keys : {key}")
